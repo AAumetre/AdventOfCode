facd50c28e4b543ee50f1742d55c41757c4fa208
Antonin Aumètre <antonin.aumetre@gmail.com>
2021-12-16 22:22:12 +0100

    Day 16

--- 2021/16.py
+++ 2021/16.py
@@ -1,91 +1,96 @@
 from functions import *
+from typing import List
+from typing import Callable
+from typing import Tuple
+from typing import Union
+import numpy as np
 
+class BitsParser:
+	def __init__(self, buff: str):
+		self.buff = buff
+		self.index = 0
+		self.versions = []
 
-def read_bits(buff: str, index: int, n: int) -> Tuple[int, int]:
-	val = int(buff[index:index+n], 2)
-	index += n
-	return val, index
+	def read_bits(self, n: int) -> int:
+		val = int(self.buff[self.index:self.index+n], 2)
+		self.index += n
+		return val
 
+	def read_chars(self, n: int) -> str:
+		ret = self.buff[self.index:self.index+n]
+		self.index += n
+		return ret
 
-def process_operator_length(buff: str, index: int, length: int) -> Tuple[List[int], int]:
-	print(f"Process operator, {length} bits to read:")
-	all_versions = []
-	end = index + length
-	while index < end:
-		versions, index = process_packet(buff, index)
-		all_versions += versions
-		print(f"	versions: {versions}")
-	return all_versions, index
-	
-def process_operator_count(buff: str, index: int, count: int) -> Tuple[List[int], int]:
-	print(f"Process operator, {count} packets to read:")
-	all_versions = []
-	for c in range(count):
-		versions, index = process_packet(buff, index)
-		all_versions += versions
-	return all_versions, index
+	def process_operator_length(self, length: int, type: int) -> int:
+		end = self.index + length
+		values = []
+		while self.index < end:
+			values.append( self.process_packet() )
+		return self.evaluate(values, type)
 
+	def process_operator_count(self, count: int, type: int) -> int:
+		values = []
+		for c in range(count):
+			values.append(self.process_packet())
+		return self.evaluate(values, type)
 
-def process_literal_value(buff: str, index: int) -> Tuple[int, int]:
-	print("Process literal value:")
-	done = False
-	val = 0
-	while not done:
-		next, index = read_bits(buff, index, 5)
-		print(f"	{bin(next)}")
-		if next >> 4 == 0:
-			done = True
-	return 0, index
-	
-def process_packet(buff: str, index: int) -> Tuple[List[int], int]:
-	if index+6 > len(buff)-1:
-		print(f"	not enough bits to read anymore")
-		return (0, len(buff)-1)
-	version, index = read_bits(buff, index, 3)
-	type_id, index = read_bits(buff, index, 3)
-	all_versions = [version]
-	print(f"version '{version}', type ID '{type_id}'")
-	
-	versions = []
-	if type_id == 4: # literal value
-		val, index = process_literal_value(buff, index)
-	else: # operator
-		length_type_id, index = read_bits(buff, index, 1)
-		if length_type_id == 0:
-			sub_packets_length, index = read_bits(buff, index, 15)
-			versions, index = process_operator_length(buff, index, sub_packets_length)
-		else:		
-			sub_packets_count, index = read_bits(buff, index, 11)
-			versions, index = process_operator_count(buff, index, sub_packets_count)
-	
-	all_versions += versions
-	return all_versions, index
+	def process_literal_value(self) -> int:
+		done = False
+		string = ""
+		while not done:
+			next = self.read_chars(5)
+			if next[0] == "0":
+				done = True
+			string += next[1:]
+		return int(string, 2)
 
+	def process_packet(self) -> int:
+		if self.index+6 > len(self.buff)-1:
+			print(f"	not enough bits to read anymore")
+			return 0
+		version = self.read_bits(3)
+		type_id = self.read_bits(3)
+		self.versions.append(version)
+
+		if type_id == 4: # literal value
+			return self.process_literal_value()
+		else: # operators
+			length_type_id = self.read_bits(1)
+			if length_type_id == 0:
+				sub_packets_length = self.read_bits(15)
+				return self.process_operator_length(sub_packets_length, type_id)
+			else:
+				sub_packets_count = self.read_bits(11)
+				return self.process_operator_count(sub_packets_count, type_id)
+
+	def evaluate(self, values: List[int], type: int) -> int:
+		if type == 0: # sum
+			return sum(values)
+		elif type == 1: # product
+			return np.prod(values)
+		elif type == 2: # min
+			return min(values)
+		elif type == 3: # max
+			return max(values)
+		elif type == 5: # [0] greater than [1]
+			return values[0] > values[1]
+		elif type == 6: # [0] less than [1]
+			return values[0] < values[1]
+		elif type == 7: # equal
+			return values[0] == values[1]
+		else:
+			print(f"ERROR: unexpected packet type ID: {type}")
+			return 0
 
 
 def main():
 	data = read_file("16.in")[0]
-	# packet: [version(3 bits)][type ID(3 bits)]
-	# ID 4: literal value, padded with leading zeros to get 4 bits blocks
-	#		each group prefixed with 1, except the last one
-	print(data)
-	
 	bin_buffer = ""
 	for h in data:
 		bin_buffer += bin(int(h, 16)).replace("0b","").zfill(4)
-	print( bin_buffer )
-	
-	index = 0
-	all_versions = []
-	#while index < len(bin_buffer)-1:
-	#	print(f"Starting a new reading from index={index} (over a total length of {len(bin_buffer)})")
-	#	versions, index = process_packet(bin_buffer, index)
-	#	print(f"All versions (in part): {versions}")
-	#	if versions != 0:
-	#		all_versions += versions
-	all_versions, index = process_packet(bin_buffer, index)
-	
-	# add all the version numbers
-	print(f"The sum of {all_versions} is {sum(all_versions)}")
 
+	bp = BitsParser(bin_buffer)
+	out = bp.process_packet()
+	print(f"The sum of all the versions is equal to {sum(bp.versions)}")
+	print(f"The evaluation of the expression gives {out}")
 main()
