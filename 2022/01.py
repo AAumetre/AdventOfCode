67569ce79e8bdb39b6fbf41034f94cfa287b8952
Antonin Aumètre <antonin.aumetre@gmail.com>
2022-12-01 08:11:11 +0100

    day 1

--- 2022/01.py
+++ 2022/01.py
@@ -3,7 +3,9 @@ from functions import *
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    data = read_file("data/01.ex")
+    calories = sorted([sum([int(i) for i in s.split(",")]) for s in ",".join(read_file("data/01.in")).split(",,")])
+    logging.info(f"The maximum number of calories carried by an elf is {calories[-1]}.")
+    logging.info(f"The sum of the three largest calorie-carrying elves is {sum(calories[-3:])}.")
 
 
 start_time = time.time_ns()
--- 2022/functions.py
+++ 2022/functions.py
@@ -20,29 +20,31 @@ from functools import reduce
 import time
 from dataclasses import dataclass
 
-def read_file(filename: str) -> List:
+
+def read_file(filename: str) -> List[str]:
     """ Returns a list of the lines in the file """
     try:
         with open(filename, 'r') as f:
             data = []
             for line in f:
-                data.append( line.strip() )
+                data.append(line.strip())
             return data
     except IOError:
         print(f"Error when opening the {filename} file.")
         exit(1)
-		
+
+
 def get_neighbor_indexes_manhattan(i: int, j: int, I: int, J: int) -> List[List[int]]:
     """ Returns the list of the valid neighbors' indexes r=1, diagonals excluded"""
     neighbors = []
-    if i-1 >= 0:
-        neighbors.append( [i-1, j] )
-    if i+1 < I:
-        neighbors.append( [i+1, j] )
-    if j-1 >= 0:
-        neighbors.append( [i, j-1] )
-    if j+1 < J:
-        neighbors.append( [i, j+1] )
+    if i - 1 >= 0:
+        neighbors.append([i - 1, j])
+    if i + 1 < I:
+        neighbors.append([i + 1, j])
+    if j - 1 >= 0:
+        neighbors.append([i, j - 1])
+    if j + 1 < J:
+        neighbors.append([i, j + 1])
     return neighbors
 
 
@@ -62,24 +64,26 @@ def get_neighbor_indexes_euclidian(data: List, i: int, j: int) -> List[Tuple[int
     if plusJ: indexes.append((i, j + 1))
     if plusJ and plusI: indexes.append((i + 1, j + 1))
     return indexes
-    
+
+
 def get_neighbor_indexes_euclidian3(point: Tuple[int, int, int]) -> List[Tuple[int, int, int]]:
     """ Returns the list of neighbors, at Euclidian distance of 1, in 3 dimensions. """
     indexes = []
     for i in [-1, 0, 1]:
         for j in [-1, 0, 1]:
             for k in [-1, 0, 1]:
-               indexes.append((point[0]+i, point[1]+j, point[2]+k))
+                indexes.append((point[0] + i, point[1] + j, point[2] + k))
     indexes.remove((point[0], point[1], point[2]))
     return indexes
-    
+
+
 def get_neighbor_indexes_euclidian4(point: Tuple[int, int, int, int]) -> List[Tuple[int, int, int, int]]:
     """ Returns the list of neighbors, at Euclidian distance of 1, in 4 dimensions. """
     indexes = []
     for i in [-1, 0, 1]:
         for j in [-1, 0, 1]:
             for k in [-1, 0, 1]:
-            	for l in [-1, 0, 1]:
-               		indexes.append((point[0]+i, point[1]+j, point[2]+k, point[3]+l))
+                for l in [-1, 0, 1]:
+                    indexes.append((point[0] + i, point[1] + j, point[2] + k, point[3] + l))
     indexes.remove((point[0], point[1], point[2], point[3]))
     return indexes
