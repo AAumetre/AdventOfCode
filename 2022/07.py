a37e2b89a44751a2d151767d49e3f44ecec294f3
Antonin Aumètre <antonin.aumetre@gmail.com>
2022-12-08 20:25:15 +0100

    fix indentation

--- 2022/07.py
+++ 2022/07.py
@@ -5,67 +5,66 @@ Node = typing.NewType("Node", None)
 
 @dataclass()
 class Node:
-	parent_: Node
-	children_: Dict[str, Node]
-	size_: int
-	
+    parent_: Node
+    children_: Dict[str, Node]
+    size_: int
+
 
 def get_size(node_: Node) -> int:
-	""" Computes the size of a node, as per  AoC definition. """
-	if node_.children_ == {}:
-		return node_.size_
-	else:
-		total_size = 0
-		for _, c in node_.children_.items():
-			total_size += get_size(c)
-		node_.size_ = total_size
-		return total_size
-		
+    """ Computes the size of a node, as per  AoC definition. """
+    if node_.children_ == {}:
+        return node_.size_
+    else:
+        total_size = 0
+        for _, c in node_.children_.items():
+            total_size += get_size(c)
+        node_.size_ = total_size
+        return total_size
+
 
 def get_dir_sizes(node_: Node) -> List[int]:
-	""" Browses a graph and returns the list of all the dir's sizes. """
-	if node_.children_ == {}:
-		return []
-	else:
-		size = [node_.size_]
-		for _, c in node_.children_.items():
-			size += get_dir_sizes(c)
-		return size
-	
+    """ Browses a graph and returns the list of all the dir's sizes. """
+    if node_.children_ == {}:
+        return []
+    else:
+        size = [node_.size_]
+        for _, c in node_.children_.items():
+            size += get_dir_sizes(c)
+        return size
+
 
 def main():
-	logging.basicConfig(level=logging.INFO)
-	data = read_file("data/07.in")
+    logging.basicConfig(level=logging.INFO)
+    data = read_file("data/07.in")
 
-	root = Node(None, {}, 0)
-	current_node = root
-	for line in data:
-		logging.debug(f">>>> {line}.")
-		if line[:4] == "$ cd":
-			if line[5:] == "/":
-				current_node = root
-			elif line[5:] == "..":
-				current_node = current_node.parent_
-			else:
-				current_node = current_node.children_[line[5:]]
-		elif line[:4] == "$ ls":
-			pass
-		else:  # we're inside an 'ls' output
-			if line[:3] == "dir":
-				current_node.children_[line[4:]] = Node(current_node, {}, 0)
-			else:
-				current_node.children_[line.split()[1]] = Node(current_node, {}, int(line.split()[0]))
-	
-	used_space = get_size(root)  # updates directory sizes
-	dir_sizes = sorted(get_dir_sizes(root))
-	small_dir_size = sum([s for s in dir_sizes if s < 100000])
-	logging.info(f"The size of all the small directories is {small_dir_size}.")
-	
-	target_space = used_space + 30000000 - 70000000
-	smallest_dir = min([d for d in dir_sizes if d >= target_space])
-	logging.info(f"The smallest dir that we should remove is of size {smallest_dir}.")
-	
-	
+    root = Node(None, {}, 0)
+    current_node = root
+    for line in data:
+        logging.debug(f">>>> {line}.")
+        if line[:4] == "$ cd":
+            if line[5:] == "/":
+                current_node = root
+            elif line[5:] == "..":
+                current_node = current_node.parent_
+            else:
+                current_node = current_node.children_[line[5:]]
+        elif line[:4] == "$ ls":
+            pass
+        else:  # we're inside an 'ls' output
+            if line[:3] == "dir":
+                current_node.children_[line[4:]] = Node(current_node, {}, 0)
+            else:
+                current_node.children_[line.split()[1]] = Node(current_node, {}, int(line.split()[0]))
+
+    used_space = get_size(root)  # updates directory sizes
+    dir_sizes = sorted(get_dir_sizes(root))
+    small_dir_size = sum([s for s in dir_sizes if s < 100000])
+    logging.info(f"The size of all the small directories is {small_dir_size}.")
+
+    target_space = used_space + 30000000 - 70000000
+    smallest_dir = min([d for d in dir_sizes if d >= target_space])
+    logging.info(f"The smallest dir that we should remove is of size {smallest_dir}.")
+
 
 start_time = time.time_ns()
 main()
--- 2022/08.py
+++ 2022/08.py
@@ -3,51 +3,51 @@ from functions import *
 
 @dataclass()
 class TreeInfo:
-	height_: int
-	visible_: bool
-	scenic_score_: int
-	
-	
+    height_: int
+    visible_: bool
+    scenic_score_: int
+
+
 def main():
-	logging.basicConfig(level=logging.INFO)
-	data = read_file("data/08.in")
-	forest = {}
-	for j in range(len(data)):
-		for i in range(len(data[0])):
-			visible = False
-			if i==0 or j==0 or i==len(data[0])-1 or j==len(data)-1:
-				visible = True
-			forest[(i,j)] = TreeInfo(int(data[j][i]), visible, 1)
-	
-	directions = [[1,0],[0,-1],[-1,0],[0,1]]
-	for j in range(1, len(data)-1):
-		for i in range(1, len(data[0])-1):
-			blocked_directions = 0
-			for d in directions:
-				look_x, look_y = i+d[0], j+d[1]
-				score_multiplier = 1
-				bumped_tree = False
-				while look_x < len(data[0]) and look_x >= 0 and look_y < len(data) and look_y >= 0:
-					if forest[(look_x, look_y)].height_ >= forest[(i,j)].height_:
-						blocked_directions += 1
-						score_multiplier = max(abs(i-look_x), abs(j-look_y))
-						bumped_tree = True
-						break
-					look_x += d[0]
-					look_y += d[1]
-				if not bumped_tree:
-					score_multiplier = max(abs(i-look_x+d[0]), abs(j-look_y+d[1]))
-				logging.debug(f"Tree {(i,j)}'s scenic score multiplier in direction {d} is {score_multiplier}.")
-				forest[(i, j)].scenic_score_ *= score_multiplier
-			if blocked_directions < 4:
-				forest[(i, j)].visible_ = True
-	
-	visible_trees = sum([tree.visible_ for tree in forest.values()])
-	logging.info(f"There are {visible_trees} visible trees in the forest.")
-	max_score = max([tree.scenic_score_ for tree in forest.values()])
-	logging.info(f"The maximum scenic score is {max_score}.")
-	
-	
+    logging.basicConfig(level=logging.INFO)
+    data = read_file("data/08.in")
+    forest = {}
+    for j in range(len(data)):
+        for i in range(len(data[0])):
+            visible = False
+            if i == 0 or j == 0 or i == len(data[0]) - 1 or j == len(data) - 1:
+                visible = True
+            forest[(i, j)] = TreeInfo(int(data[j][i]), visible, 1)
+
+    directions = [[1, 0], [0, -1], [-1, 0], [0, 1]]
+    for j in range(1, len(data) - 1):
+        for i in range(1, len(data[0]) - 1):
+            blocked_directions = 0
+            for d in directions:
+                look_x, look_y = i + d[0], j + d[1]
+                score_multiplier = 1
+                bumped_tree = False
+                while look_x < len(data[0]) and look_x >= 0 and look_y < len(data) and look_y >= 0:
+                    if forest[(look_x, look_y)].height_ >= forest[(i, j)].height_:
+                        blocked_directions += 1
+                        score_multiplier = max(abs(i - look_x), abs(j - look_y))
+                        bumped_tree = True
+                        break
+                    look_x += d[0]
+                    look_y += d[1]
+                if not bumped_tree:
+                    score_multiplier = max(abs(i - look_x + d[0]), abs(j - look_y + d[1]))
+                logging.debug(f"Tree {(i, j)}'s scenic score multiplier in direction {d} is {score_multiplier}.")
+                forest[(i, j)].scenic_score_ *= score_multiplier
+            if blocked_directions < 4:
+                forest[(i, j)].visible_ = True
+
+    visible_trees = sum([tree.visible_ for tree in forest.values()])
+    logging.info(f"There are {visible_trees} visible trees in the forest.")
+    max_score = max([tree.scenic_score_ for tree in forest.values()])
+    logging.info(f"The maximum scenic score is {max_score}.")
+
+
 start_time = time.time_ns()
 main()
 logging.info(f"Duration: {(time.time_ns() - start_time) / 10 ** 9} s")
