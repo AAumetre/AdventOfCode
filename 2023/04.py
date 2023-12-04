b090c63b4c9cacda5e36af6a78b85f23ac60d2b3
Antonin Aumètre <antonin.aumetre@gmail.com>
2023-12-04 22:26:09 +0100

    day 4 part 2

--- 2023/04.py
+++ 2023/04.py
@@ -6,13 +6,17 @@ def main():
     data = read_file("data/04.in")
 
     sum_points = 0
-    for line in data:
+    card_copies = [1]*len(data)
+    for index, line in enumerate(data):
         winning_numbers = set([int(n) for n in line.split(": ")[1].split(" | ")[0].split()])
         card_numbers = set([int(n) for n in line.split(": ")[1].split(" | ")[1].split()])
         c = len(winning_numbers.intersection(card_numbers))
         if c > 0:
             sum_points += pow(2, c-1)
+            for i in range(1, c+1):
+                card_copies[index+i] += card_copies[index]
     logging.info(f"Part 1: {sum_points=}")
+    logging.info(f"Part 2: {sum(card_copies)=}")
 
 
 start_time = time.time_ns()
